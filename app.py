bc4c6381fdd9516a2416da3fa148f6a60e7d7a4a
Emanuel Andersson <Gaarru@outlook.com>
2024-02-11 04:51:54 +0100

    Skapat en webbsida /admin samt lagt till så vi kan radera nyhetsbrev ifrån databasen via webbsidan

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from models import db, seedData
+from flask import Flask, render_template, request, redirect  # importerat även dessa: render_template, request, redirect
+from models import db, seedData, NewsletterEmails # importerar även NewsletterEmails från models
 from flask_migrate import Migrate, upgrade
 from areas.site.sitePages import siteBluePrint
 from areas.products.productPages import productBluePrint
@@ -13,6 +13,21 @@ db.init_app(app)
 migrate = Migrate(app, db)
 # user_manager.app = app
 # user_manager.init_app(app,db,User)
+
+@app.route('/admin') # Tar med mig NewsletterEmails till admin page
+def admin():
+    return render_template("admin.html", values=NewsletterEmails.query.all())
+
+@app.route('/delete', methods=['POST'])  ## Skickar till denna sida som söker fram emailen i databasen
+def delete_email():
+    email_id = int(request.form['email_id'])
+    email_to_delete = NewsletterEmails.query.get(email_id)
+    if email_to_delete:
+        db.session.delete(email_to_delete)
+        db.session.commit()
+        return redirect('/admin')  # Skickar tillbaka till adminsidan efter, raderingen
+    else:
+        return "Email not found", 404
 
 app.register_blueprint(siteBluePrint)
 app.register_blueprint(productBluePrint)
